1cfb8fc822f2b0baaab951ae48e33294f1287a8e
Rohit Vishwakarma <Rohit993583@gmail.com>
2022-10-22 12:26:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let t = false;
   const [paragraph, setPara] = useState("");
   const clickHandler = ()=>{
+    t = true;
     setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
   }
 
   return (
     <div id="main">
-      <p id="para">{paragraph}</p>
+      {(!t) ? <p id="para">{paragraph}</p> : ""}
       <button id="click" onClick={()=>clickHandler()}>Click</button>
     </div>
   );
