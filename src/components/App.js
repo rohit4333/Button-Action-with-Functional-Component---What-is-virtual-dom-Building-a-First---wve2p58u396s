30861396a968d43e8213985223a5e6f1ae4a9b0f
Rohit Vishwakarma <Rohit993583@gmail.com>
2022-10-22 20:34:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,49 @@
 import React, {Component, useState} from "react";
+
 import "./../styles/App.css";
 
-function App() {
-  let t = false;
-  const [paragraph, setPara] = useState("");
-  const clickHandler = ()=>{
-    t = true;
-    setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-  }
+ 
 
-  return (
-    <div id="main">
-      
-      <button id="click" onClick={()=>clickHandler()}>Click</button>
-      {(!t) ? <p id="para">{paragraph}</p> : <p></p>}
-    </div>
-  );
+ 
+
+function App() {
+
+ const [show, setShow] = useState(false)
+
+ 
+
+ const handleClick = () => {
+
+       // console.log('djgfdj')
+
+       setShow(true)
+
+ }
+
+ return (
+
+   <div id="main">
+
+      <button id="click" type="submit" onClick={()=>{handleClick()}}>Click</button>
+
+      {show === true ? (
+
+       <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+
+       </p>
+
+     ) : undefined }
+
+   </div>
+
+ );
+
 }
 
+ 
+
+ 
 
 export default App;
+
+
